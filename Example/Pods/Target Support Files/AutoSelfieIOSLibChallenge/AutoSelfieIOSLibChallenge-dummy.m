e0b9eb60db05c5dcc76a2e52863ddd0311c381d7
Michel Sievers <michel@easy-target.eu>
2021-06-21 16:37:29 +0200

    chore: add initial commit

---
+++ Example/Pods/Target Support Files/AutoSelfieIOSLibChallenge/AutoSelfieIOSLibChallenge-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AutoSelfieIOSLibChallenge : NSObject
+@end
+@implementation PodsDummy_AutoSelfieIOSLibChallenge
+@end
---
+++ Example/Pods/Target Support Files/Pods-AutoSelfieIOSLibChallenge_Example/Pods-AutoSelfieIOSLibChallenge_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AutoSelfieIOSLibChallenge_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AutoSelfieIOSLibChallenge_Example
+@end
